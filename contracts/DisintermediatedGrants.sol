57bfb15807546ef8614f6af3a65a87efdabbd194
k4m4 <nikolaskam@gmail.com>
2022-05-12 08:44:04 -0400

    Use `memory` for donation in `proposeGrant`

--- contracts/DisintermediatedGrants.sol
+++ contracts/DisintermediatedGrants.sol
@@ -119,7 +119,7 @@ contract DisintermediatedGrants is Ownable {
         address _recipient,
         uint256 _amount
     ) public onlyOwner {
-        Donation storage donation = donations[_donationId];
+        Donation memory donation = donations[_donationId];
         require(donation.amount - donation.disbursedAmount >= _amount, "donation cannot cover full grant amount");
 
         Grant memory grant = Grant({
