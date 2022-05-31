0a37c288b520fb9ed13c344271be8b164889d1cc
k4m4 <nikolaskam@gmail.com>
2022-05-31 13:31:25 +0300

    Update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,10 +7,7 @@ async function main() {
     console.log("Account balance:", (await deployer.getBalance()).toString())
 
     const DisintermediatedGrants = await ethers.getContractFactory("DisintermediatedGrants")
-    const disintermediatedGrants = await DisintermediatedGrants.deploy(
-        process.env.MULTISIG_ADDRESS,
-        process.env.GRACE_PERIOD
-    )
+    const disintermediatedGrants = await DisintermediatedGrants.deploy(process.env.MULTISIG_ADDRESS)
 
     console.log("DisintermediatedGrants address:", disintermediatedGrants.address)
 }
