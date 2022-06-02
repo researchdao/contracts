6d1f064c9281e287945c95f8bddc9fa796126406
k4m4 <nikolaskam@gmail.com>
2022-06-02 10:49:41 +0300

    Run formatter on all JS files

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,18 @@
-require("@nomiclabs/hardhat-waffle");
-require("solidity-coverage");
-require('dotenv').config();
+require("@nomiclabs/hardhat-waffle")
+require("solidity-coverage")
+require("dotenv").config()
 
-const HARMONY_PRIVATE_KEY = process.env.HARMONY_PRIVATE_KEY;
+const HARMONY_PRIVATE_KEY = process.env.HARMONY_PRIVATE_KEY
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-    const accounts = await hre.ethers.getSigners();
+    const accounts = await hre.ethers.getSigners()
 
     for (const account of accounts) {
-        console.log(account.address);
+        console.log(account.address)
     }
-});
+})
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
@@ -23,7 +23,7 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 module.exports = {
     gasPrice: 0,
     solidity: {
-        version: '0.8.4',
+        version: "0.8.4",
         settings: {
             optimizer: {
                 enabled: true,
@@ -31,23 +31,23 @@ module.exports = {
             },
             outputSelection: {
                 "*": {
-                    "*": ["storageLayout"]
-                }
-            }
-        }
+                    "*": ["storageLayout"],
+                },
+            },
+        },
     },
     networks: {
         devnet: {
-            url: 'https://api.s0.ps.hmny.io',
+            url: "https://api.s0.ps.hmny.io",
             accounts: [`0x${HARMONY_PRIVATE_KEY}`],
         },
         testnet: {
-            url: 'https://api.s0.b.hmny.io',
+            url: "https://api.s0.b.hmny.io",
             accounts: [`0x${HARMONY_PRIVATE_KEY}`],
         },
         mainnet: {
-            url: 'https://api.harmony.one',
+            url: "https://api.harmony.one",
             accounts: [`0x${HARMONY_PRIVATE_KEY}`],
         },
     },
-};
+}
