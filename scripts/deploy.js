4a4cfef94451c0fc0a9bc171776d24e02ee65fd2
k4m4 <nikolaskam@gmail.com>
2022-06-12 17:33:13 +0300

    Log additional relevant info in deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,14 +3,17 @@ require("dotenv").config()
 async function main() {
     const [deployer] = await ethers.getSigners()
 
+    const MULTISIG_ADDRESS = process.env.MULTISIG_ADDRESS
+    const MAX_DONATION_GRACE_PERIOD = process.env.MAX_DONATION_GRACE_PERIOD
+
     console.log("Deploying contracts with the account:", deployer.address)
     console.log("Account balance:", (await deployer.getBalance()).toString())
 
+    console.log("Multisig address:", MULTISIG_ADDRESS)
+    console.log("Maximum donation grace period (in blocks):", MAX_DONATION_GRACE_PERIOD)
+
     const DisintermediatedGrants = await ethers.getContractFactory("DisintermediatedGrants")
-    const disintermediatedGrants = await DisintermediatedGrants.deploy(
-        process.env.MULTISIG_ADDRESS,
-        process.env.MAX_DONATION_GRACE_PERIOD
-    )
+    const disintermediatedGrants = await DisintermediatedGrants.deploy(MULTISIG_ADDRESS, MAX_DONATION_GRACE_PERIOD)
 
     console.log("DisintermediatedGrants address:", disintermediatedGrants.address)
 }
